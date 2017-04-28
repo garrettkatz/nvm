0e0968b08adff0cecafc562a0248bf7d1a88b6ef
garrettkatz <garrett.katz@gmail.com>
2017-04-28 18:37:21 -0400

    2008 galis network prototyped

--- galis_network.py
+++ galis_network.py
@@ -1,32 +1,46 @@
 """
 Implementation of GALIS neural network model for sequence learning
+(Sylvester 2008)
+Confusion about 2016 (ISM):
+for V, uses a_old*a_new.T, shouldn't it be a_new*a_old.T?
+confusing that the same t is used, but multiple stages in the update process.  is theta updated three times, before h, between h and f, and after f?  or only two?  and when? are all three of these thetas referred to as theta(t)?
 """
 
 class GALISNN:
-    def __init__(self, network_size, k, k_theta, k_w, history=2):
-        self.N = network_size
-        self.k = k
+    def __init__(self, N, k_d, k_theta, k_w, beta_1, beta_2, history=2):
+        self.N = N
+        self.k_d = k_d
         self.k_theta = k_theta
         self.k_w = k_w
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.history = history
         self.W = np.zeros((self.N, self.N))
         self.V = np.zeros((self.N, self.N))
-        self.a
-        self.theta
-        self.history = history
-        self.tick_mark = 0
-    def activate(self, external_input):
-        theta = self.a[:,[self.tick_mark]]
-        theta = (1-self.k_theta)*theta
-        a = self.a[:,[self.tick_mark]]
-        h = self.V.dot(a) - theta + external_input
-        a = np.sign(h)
-        theta = (1-self.k_theta)*theta
-        f = self.W.dot(a) - theta + external_input
-        a = np.sign(f)
-        theta = (1-self.k_theta)*theta
-    def learn(self, a_old, a_new):
+        self.a = np.zeros((self.N, self.history))
+        self.theta = np.zeros((self.N, self.history))
+        self.t = 0
+    def activate(self):
         """
-        Temporally associates activation patterns a_old and a_new
+        Update the network activations
         """
-        self.W = (1-self.k)*self.W + (1/self.N)*a_new*a_new.T*(1-np.eye(self.N))
-        self.V = (1-self.k)*self.V + (1/self.N)*a_old*a_new.T
+        a = self.a[:,[self.t]]
+        theta = self.theta[:,[self.t]]
+        h = self.beta_1*self.W.dot(a) + self.beta_2*self.V.dot(a) - theta
+        a_new = np.sign(h) + (h==0)*a
+        theta_new = (1-self.k_theta)*theta + (a_new==a)*self.k_w*a
+        self.t = (self.t + 1) % self.history
+        self.a[:,[self.t]] = a_new
+        self.theta[:,[self.t]] = theta_new
+    def associate(self):
+        """
+        Temporally associate past two activity patterns
+        """
+        a_new = self.a[:,[self.t]]
+        a_old = self.a[:,[(self.t - 1) % self.history]]
+        self.W = (1-self.k_d)*self.W + (1/self.N)*a_new*a_new.T*(1-np.identity(self.N))
+        self.V = (1-self.k_d)*self.V + (1/self.N)*a_new*a_old.T
+
+
+if __name__ == '__main__':
+    pass
