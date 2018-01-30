92e66e2e4607d0b7e302cfa2f2d0371a203eeb92
garrettkatz <garrett.katz@gmail.com>
2018-01-30 17:23:43 -0500

    reasonable local baseline in seq_scratch, still cracking transit linprog

--- seq_scratch.py
+++ seq_scratch.py
@@ -5,22 +5,26 @@ N = 32
 K = 4
 T = 4*K
 
-V_seq = np.sign(np.random.rand(N,K) - .5) * (.9 + .1*np.random.rand(N,K))
-# X = V_seq.copy()
-# Y = 1*(V_seq[:, np.arange(1,K+1) % K] - V_seq)
-a = 0.05
-X = (1-a)*V_seq[:,:-1] + a*V_seq[:, 1:]
-Y = a*V_seq[:,:-1] + (1-a)*V_seq[:, 1:]
+pad = .1
+noise = 0
+V_seq = np.sign(np.random.rand(N,K) - .5) * ((1-pad) + noise*np.random.rand(N,K))
+X = V_seq.copy()
+Y = np.roll(V_seq, -1, axis=1)
+
+# a = 0.05
+# X = (1-a)*V_seq[:,:-1] + a*V_seq[:, 1:]
+# Y = a*V_seq[:,:-1] + (1-a)*V_seq[:, 1:]
 
 # WX = sY
 # X.T W.T = sY.T
-W = np.linalg.lstsq(X.T, np.arctanh(Y).T, rcond=None)[0].T
+# W = np.linalg.lstsq(X.T, np.arctanh(Y).T, rcond=None)[0].T
+W = np.arctanh(Y).dot(X.T)
 
 # # W = 1.1*np.eye(N)
 # #np.random.randn(N,N)
 
 V = np.empty((N,T))
-V[:,[0]] = V_seq[:,[0]]
+V[:,[0]] = V_seq[:,[0]]*((-1.)**(np.random.rand(N,1) < 0.0))
 for t in range(1,T):
     V[:,[t]] = np.tanh(W.dot(V[:,[t-1]]))
 
@@ -41,12 +45,14 @@ V_t = traj_signs(V)
 # print(np.sign(V_seq) == np.sign(V_t))
 # print((np.sign(V_seq) == np.sign(V_t)).all())
 
-plt.subplot(1,4,1)
+plt.subplot(1,5,1)
 plt.imshow(wsc(W),cmap="Greys")
-plt.subplot(1,4,2)
+plt.subplot(1,5,2)
 plt.imshow(vsc(V),cmap="Greys")
-plt.subplot(1,4,3)
+plt.subplot(1,5,3)
 plt.imshow(vsc(traj_signs(V)),cmap="Greys")
-plt.subplot(1,4,4)
+plt.subplot(1,5,4)
 plt.imshow(vsc(V_seq),cmap="Greys")
+plt.subplot(1,5,5)
+plt.imshow(wsc(X.T.dot(X)),cmap="Greys")
 plt.show()
--- transit_linprog.py
+++ transit_linprog.py
@@ -2,90 +2,121 @@ import numpy as np
 import scipy.optimize as so
 import matplotlib.pyplot as mp
 
-np.set_printoptions(linewidth=200)
+np.set_printoptions(linewidth=200, precision=3)
 
-N = 4
-K = 2
-T = 4*K
+N = 32
+K = 4
+D = 4
+# D = 4/N
 
-V_seq = np.sign(np.random.rand(N,K) - .5)
-V_x = V_seq[:,:-1]
-V_y = V_seq[:,1:]
-P = V_x.shape[1]
-print(V_x.T)
-print(V_y.T)
+num_trials = 1
+num_feas = 0
+for trial in range(num_trials):
 
-# linprog
-W = np.empty((N,N))
-w_ii = 1.25
-a = np.sqrt(1. - 1./w_ii)
-z = np.arctanh(a) - w_ii*a
-stinq = 0.001 # strict inequality
-for i in range(1):
-    # minimize c^T x subject to
-    # A_ub x <= b_ub
-    # A_eq x == b_eq
-    # returns object with field 'x'
-    c = np.random.randn(N)
-    A_ub = np.empty((P,N))
-    b_ub = np.empty((P,1))
-    for p in range(P):
-        # openings
-        if np.sign(V_x[i,p]) != np.sign(V_y[i,p]):
-            A_ub[p,:] = np.sign(V_x[i,p])*V_x[:,p]
-            b_ub[p,0] = -np.fabs(z) - stinq
-        else:
-            A_ub[p,:] = -np.sign(V_x[i,p])*V_x[:,p]
-            b_ub[p,0] = np.fabs(z) - stinq
-    # # zero diagonal
-    A_eq=np.zeros((1,N))
-    A_eq[0,i] = 1.
-    b_eq = 0.
-    bounds = (-1,1) # defaults are non-negative
-    result = so.linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='simplex', callback=None, options=None)
-    print(A_eq, b_eq)
-    print(A_ub, b_ub)
-    print(c.T)
-    print(result.x, result.success, result.message)
-    W[i,:] = result.x
-    W[i,i] = w_ii
+    # V_seq = np.sign(np.random.rand(N,K) - .5)
+    V_seq = np.ones((N,K))
+    for k in range(1,K):
+        # i_delta = (np.random.rand(N) < D)
+        i_delta = np.random.permutation(N)[:D]
+        print(i_delta)
+        V_seq[:,k] = V_seq[:,k-1]
+        V_seq[i_delta,k] *= -1
+        # V_seq[~i_delta,k] = V_seq[~i_delta,k-1]
 
-# print(W)
-# # WX = sY
-# # X.T W.T = sY.T
-# W = np.linalg.lstsq(X.T, np.arctanh(Y).T, rcond=None)[0].T
+    # # open
+    # V_x = V_seq[:,:-1]
+    # V_y = V_seq[:,1:]
+    # P = V_x.shape[1]
 
-# # # W = 1.1*np.eye(N)
-# # #np.random.randn(N,N)
+    # closed
+    V_x = V_seq
+    V_y = np.roll(V_seq, -1, axis=1)
+    P = V_x.shape[1]
+    
+    # linprog
+    W = np.empty((N,N))
+    w_ii = 5
+    a = np.sqrt(1. - 1./w_ii)
+    z = np.arctanh(a) - w_ii*a
+    stinq = 0.001 # strict inequality
+    successes, messages = [], []
+    for i in range(N):
+        # minimize c^T x subject to
+        # A_ub x <= b_ub
+        # A_eq x == b_eq
+        # returns object with field 'x'
+        c = np.random.randn(N)
+        A_ub = np.empty((P,N))
+        b_ub = np.empty((P,1))
+        for p in range(P):
+            # openings
+            if np.sign(V_x[i,p]) != np.sign(V_y[i,p]):
+                A_ub[p,:] = np.sign(V_x[i,p])*V_x[:,p]
+                b_ub[p,0] = -np.fabs(z) - stinq
+            else:
+                A_ub[p,:] = -np.sign(V_x[i,p])*V_x[:,p]
+                b_ub[p,0] = np.fabs(z) - stinq
+        # # zero diagonal
+        A_eq=np.zeros((1,N))
+        A_eq[0,i] = 1.
+        b_eq = 0.
+        bounds = 2*np.array([-1,1]) # defaults are non-negative
+        result = so.linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='interior-point', callback=None, options=None)
+        # print(A_eq, b_eq)
+        # print(A_ub, b_ub)
+        # print(c.T)
+        # print(result.x, result.success, result.message)
+        successes.append(result.success)
+        messages.append(result.message)
+        W[i,:] = result.x
+        W[i,i] = w_ii
+    
+    # print(W)
+    if not all(successes):
+        for i in range(N):
+            if not successes[i]: print("%d: %s"%(i, messages[i]))
+    else:
+        print("feasible transits")
+        num_feas += 1
 
-# V = np.empty((N,T))
-# V[:,[0]] = V_seq[:,[0]]
-# for t in range(1,T):
-#     V[:,[t]] = np.tanh(W.dot(V[:,[t-1]]))
+print("%d of %d feasible!"%(num_feas,num_trials))
 
-# def wsc(X):
-#     return (X-X.min())/(X.max()-X.min())
-# def vsc(V):
-#     return .5*(V+1.)
-# def traj_signs(V):
-#     V_s = [np.sign(V[:,[0]])]
-#     for j in range(1,V.shape[1]):
-#         if (np.sign(V[:,[j]]) != np.sign(V_s[-1])).any():
-#             V_s.append(np.sign(V[:,[j]]))
-#     return np.concatenate(V_s,axis=1)
+print(((W - w_ii*np.eye(N)).min(),(W - w_ii*np.eye(N)).max()))
 
-# V_t = traj_signs(V)
-# # print(V_t)
-# # print(V_seq)
-# # print(np.sign(V_seq) == np.sign(V_t))
-# # print((np.sign(V_seq) == np.sign(V_t)).all())
+T = K*D
+V = np.empty((N,T))
+V[:,[0]] = V_seq[:,[0]]
+for t in range(1,T):
+    V[:,[t]] = np.tanh(W.dot(V[:,[t-1]]))
 
-# mp.subplot(1,4,1)
-# mp.imshow(wsc(W),cmap="Greys")
-# mp.subplot(1,4,2)
-# mp.imshow(vsc(V),cmap="Greys")
-# mp.subplot(1,4,3)
-# mp.imshow(vsc(traj_signs(V)),cmap="Greys")
-# mp.subplot(1,4,4)
-# mp.imshow(vsc(V_seq),cmap="Greys")
-# mp.show()
+def wsc(X):
+    return (X-X.min())/(X.max()-X.min())
+def vsc(V):
+    return .5*(V+1.)
+def traj_signs(V):
+    V_s = [np.sign(V[:,[0]])]
+    for j in range(1,V.shape[1]):
+        if (np.sign(V[:,[j]]) != np.sign(V_s[-1])).any():
+            V_s.append(np.sign(V[:,[j]]))
+    return np.concatenate(V_s,axis=1)
+
+V_ts = traj_signs(V)
+# # print(V_ts)
+print("Training:")
+print(V_seq.T)
+# # print(np.sign(V_seq) == np.sign(V_ts))
+# # print((np.sign(V_seq) == np.sign(V_ts)).all())
+
+mp.subplot(1,4,1)
+mp.imshow(wsc(W),cmap="Greys")
+mp.title("W")
+mp.subplot(1,4,2)
+mp.imshow(vsc(V),cmap="Greys")
+mp.title("Actual")
+mp.subplot(1,4,3)
+mp.imshow(vsc(traj_signs(V)),cmap="Greys")
+mp.title("sign(Actual)")
+mp.subplot(1,4,4)
+mp.imshow(vsc(V_seq),cmap="Greys")
+mp.title("Training")
+mp.show()
