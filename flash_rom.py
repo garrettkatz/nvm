ec3dc2be87a11b4f05707db4ccfa838e1de251ba
garrettkatz <garrett.katz@gmail.com>
2018-02-09 21:54:27 -0500

    confirmed flash_rom on instruction load

--- flash_rom.py
+++ flash_rom.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from tokens import LAYERS, DEVICES, N_LAYER, TOKENS, get_token
-from gates import N_GATES, N_HGATES, N_GH, PAD, default_gates
+from gates import N_GATES, N_HGATES, N_GH, PAD, GATE_INDEX, default_gates
 
 np.set_printoptions(linewidth=200, formatter = {'float': lambda x: '% .0f'%x})
 
@@ -30,18 +31,24 @@ def add_transit(X, Y, cpu_old, cpu_new):
     """
     X.append(cpu_old)
     Y.append(cpu_new)
-    return cpu_new
+    return cpu_new.copy()
+    
+def check_deterministic(X, Y):
+    """
+    Check for deterministic X ~~> Y transitions (identical X need identical Y)
+    """
+    for j in range(X.shape[1]):
+        for k in range(X.shape[1]):
+            if (X[:,j]==X[:,k]).all() and (Y[:,j]!=Y[:,k]).any():
+                return False
+    return True
     
 def flash_rom(X, Y, verbose=True):
     """
     Construct W that transitions states in X to corresponding states in Y
-    X, Y are lists, with cpu activity states as items
+    X, Y are arrays, with cpu activity states as columns
     To deal with low-rank X, each transition uses an intermediate hidden step
     """
-
-    # Concatenate for linear algebra
-    X = np.concatenate(X,axis=1)
-    Y = np.concatenate(Y,axis=1)
 
     # for low-rank X, get coefficients A of X's column space
     _, sv, A = np.linalg.svd(X, full_matrices=False)
@@ -55,11 +62,21 @@ def flash_rom(X, Y, verbose=True):
         X[N_GH:,:]
     ), axis=0)
 
+    if verbose:
+        print("!! Flashing !!")
+        print("Deterministic?")
+        print(check_deterministic(X,Y))
+        print("X,Z,Y shapes and ranks:")
+        for M in [X, Z, Y]:
+            print(M.shape)
+            print(np.linalg.matrix_rank(M))
+
     # solve linear equations
     X = np.concatenate((X, Z), axis=1)
-    Y = np.concatenate((Z[:N_GH,:], Y[:N_GH,:]), axis=1
+    Y = np.concatenate((Z[:N_GH,:], Y[:N_GH,:]), axis=1)
     W = np.linalg.lstsq(X.T, np.arctanh(Y).T, rcond=None)[0].T    
-    if verbose: print("Flash residual = %f"%np.fabs(Y - np.tanh(W.dot(X))).max())
+    if verbose:
+        print("Flash residual = %f"%np.fabs(Y - np.tanh(W.dot(X))).max())
 
     return W
 
@@ -69,6 +86,9 @@ cop = lambda t, f: [(t,f,"U"),(t,f,"C")]
 # gates for full memory (value and hidden) layer updates
 memu = [("MEM"+a,"MEM"+b,"U") for a in ["1","2"] for b in ["1","2"]]
 
+# operation registers
+opregs = ["OPCODE","OPERAND1","OPERAND2"]
+
 ######## FLASH ROM #########
 
 X, Y = [], [] # growing lists of transitions
@@ -76,11 +96,31 @@ v0 = cpu_state() # beginning of clock cycle
 
 ### Load instructions from mem into cpu, one operand at a time
 v = v0
-for op_ in ["OPCODE","OPERAND1","OPERAND2"]:
-    v = add_transit(X, Y, v, cpu_state(gates = memu + cop(op_,"MEM1")))
-v_ready = add_transit(X, Y, v, cpu_state(gates = []))
+for reg in opregs:
+    v = add_transit(X, Y, v, cpu_state(ungate = memu + cop(reg,"MEM1")))
+v = add_transit(X, Y, v, cpu_state(ungate = [("GATES",reg,"U") for reg in opregs]))
+
+### ready to execute instruction
+v_ready = v 
 
 # ### SET instruction
 # v1_set = 
 # add_transit(X, Y, v0, cpu_state(
 #     gates
+
+### Return to beginning state for next clock cycle
+add_transit(X, Y, v, v0)
+
+### Flash to ROM
+X = np.concatenate(X,axis=1)
+Y = np.concatenate(Y,axis=1)
+
+W = flash_rom(X, Y, verbose=True)
+
+plt.subplot(1,3,1)
+plt.imshow(np.kron((X-X.min())/(X.max()-X.min()),np.ones((1,3))), cmap='gray')
+plt.subplot(1,3,2)
+plt.imshow(np.kron((Y-Y.min())/(Y.max()-Y.min()),np.ones((1,3))), cmap='gray')
+plt.subplot(1,3,3)
+plt.imshow((W-W.min())/(W.max()-W.min()), cmap='gray')
+plt.show()
