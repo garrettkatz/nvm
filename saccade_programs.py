53eb0f49fffcf3963eb80bfbbbb08a350f08c4dd
garrettkatz <garrett.katz@gmail.com>
2018-07-06 13:35:47 -0400

    workaround for pef->ip linkage

--- saccade_programs.py
+++ saccade_programs.py
@@ -9,17 +9,14 @@ from nvm_assembler import assemble
 from nvm_linker import link
 from nvm_net import NVMNet
 
-# New version that uses comparison
-# Not implemented in syngen quite yet
+# New version using pef
 aas_program = {"aas":"""
 
 loop:   mov fef center
         mov sc on
         mov sc off
-wait:   cmp tc cross
-        jie cross
-        jmp wait
-cross:  jmp tc
+cross:  mov pef tc
+        jmp pef
 left:   mov fef right
         jmp look
 right:  mov fef left
@@ -30,6 +27,27 @@ look:   mov sc on
         exit
 
 """}
+
+# # New version that uses comparison
+# aas_program = {"aas":"""
+
+# loop:   mov fef center
+#         mov sc on
+#         mov sc off
+# wait:   cmp tc cross
+#         jie cross
+#         jmp wait
+# cross:  jmp tc
+# left:   mov fef right
+#         jmp look
+# right:  mov fef left
+#         jmp look
+# look:   mov sc on
+#         mov sc off
+#         jmp loop
+#         exit
+
+# """}
 
 # Old version that doesn't depend on comparison
 '''
@@ -53,11 +71,11 @@ look:   mov sc on
 """}
 '''
 
-def make_fef(pad, activator, rows, columns):
+def make_ef(name, pad, activator, rows, columns):
     dim = min(rows, columns)
 
     act = activator(pad, rows*columns)
-    fef_coder = Coder(act)
+    ef_coder = Coder(act)
 
     Y, X = np.mgrid[:rows,:columns] # transpose for bitmap
     R = .1
@@ -67,13 +85,15 @@ def make_fef(pad, activator, rows, columns):
     # right = act.off + (act.on-act.off)*np.exp(-((X-1.*columns)**2 + (Y-.5*rows)**2)/(R*dim)**2)
     # Binary
     center = act.off + (act.on-act.off)*((X-.5*columns)**2 + (Y-.5*rows)**2 < (R*dim)**2)
-    left = act.off + (act.on-act.off)*((X-.0*columns)**2 + (Y-.5*rows)**2 < (R*dim)**2)
-    right = act.off + (act.on-act.off)*((X-1.*columns)**2 + (Y-.5*rows)**2 < (R*dim)**2)
+    left = act.off + (act.on-act.off)*((X-.125*columns)**2 + (Y-.5*rows)**2 < (R*dim)**2)
+    right = act.off + (act.on-act.off)*((X-.875*columns)**2 + (Y-.5*rows)**2 < (R*dim)**2)
 
-    fef_coder.encode("center", center.reshape((rows*columns,1)))
-    fef_coder.encode("left", left.reshape((rows*columns,1)))
-    fef_coder.encode("right", right.reshape((rows*columns,1)))
-    return Layer("fef", (rows,columns), act, fef_coder)
+    ef_coder.encode("cross", center.reshape((rows*columns,1)))
+    ef_coder.encode("center", center.reshape((rows*columns,1)))
+    ef_coder.encode("left", left.reshape((rows*columns,1)))
+    ef_coder.encode("right", right.reshape((rows*columns,1)))
+
+    return Layer(name, (rows,columns), act, ef_coder)
 
 def make_sc(pad, activator, rows, columns):
 
@@ -100,15 +120,27 @@ def make_saccade_nvm(activator_label):
     
     devices = {
         "tc": Layer("tc", layer_shape, act, Coder(act)),
-        "fef": make_fef(pad, activator, 68, 96),
+        "fef": make_ef("fef", pad, activator, 68, 96),
+        "pef": make_ef("pef", pad, activator, 68, 96),
         "sc": make_sc(pad, activator, 5, 5)}
 
-    # assemble and link programs
     shapes = {"gh": (32,16)}
     nvmnet = NVMNet(layer_shape, pad, activator, learning_rule, devices, shapes=shapes)
+
+    # assemble and link programs
     for name, program in aas_program.items():
         nvmnet.assemble(program, name, verbose=1)
     nvmnet.link(verbose=2)
+
+    # redo pef/fef linkages with special learning rule
+    # pef -> ip
+    X = np.concatenate([nvmnet.layers["pef"].coder.encode(tok)
+        for tok in ["left","right","cross"]], axis=1)
+    Y = np.concatenate([nvmnet.layers["ip"].coder.encode(tok)
+        for tok in ["left","right","cross"]], axis=1)
+    nvmnet.weights[("ip","pef")] = nvmnet.layers["ip"].activator.g(Y).dot(
+        X.T / (X**2).sum(axis=0)[:, np.newaxis])
+    nvmnet.biases[("ip","pef")][:] = 0
 
     # initialize layers
     nvmnet.activity["ip"] = nvmnet.layers["ip"].coder.encode(name) # program pointer
@@ -130,21 +162,22 @@ if __name__ == "__main__":
 
     history = []
     start_t = []
-    int_sched = [20, 80]
-    tc_sched = [60, 110, 120]
-    for t in range(tc_sched[2]*3):
+    int_sched = [20, 20]
+    tc_sched = [60, 110, 200]
+    for t in range(tc_sched[2]):
 
         # brief interrupt
-        if t == int_sched[0]:
+        if t >= int_sched[0]:
             nvmnet.activity["gi"] = nvmnet.layers["gi"].coder.encode("pause")
-        if t == int_sched[1]:
+        if t >= int_sched[1]:
             nvmnet.activity["gi"] = nvmnet.layers["gi"].coder.encode("quiet")
 
         ### occassionally change tc
         if t > 0 and t % tc_sched[2] in tc_sched[:2]:
         # if np.random.rand() < 1./100:
             if t % tc_sched[2] == tc_sched[0]:
-                tok = ["left","right"][np.random.randint(2)]
+                # tok = ["left","right"][np.random.randint(2)]
+                tok = "right"
             if t % tc_sched[2] == tc_sched[1]:
                 tok = "cross"
             nvmnet.activity["tc"] = nvmnet.layers["tc"].coder.encode(tok) # maybe face appears
@@ -159,9 +192,9 @@ if __name__ == "__main__":
             # raw_input(".")
         if nvmnet.at_exit():
             break
-        nvmnet.tick()
 
         history.append(dict(nvmnet.activity))
+        nvmnet.tick()
         
     ### raster plot
     A = np.zeros((sum([
