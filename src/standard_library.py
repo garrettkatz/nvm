bc35ade3dcdff34c52e342e2c6a3bf8f75006f3b
garrettkatz <garrett.katz@gmail.com>
2018-07-16 16:58:46 -0400

    prototyped stl logic routines

--- src/standard_library.py
+++ src/standard_library.py
@@ -1,4 +1,47 @@
 """
 NVM standard library routines for logic and arithmetic
+r0, r1, ... are replaced with register names in order
 """
 
+stl_template = """
+                exit
+
+# logical-not of r0
+# overwrites r0 with the result
+stl.not:        cmp {r0} true
+                jie stl.not.false
+                mov {r0} true
+                ret
+stl.not.false:  mov {r0} false
+                ret
+    
+# logical-and of r0 and r1
+# overwrites r0 with result
+stl.and:        cmp {r0} false
+                jie stl.and.false
+                cmp {r1} false
+                jie stl.and.false
+                mov {r0} true
+                ret
+stl.and.false:  mov {r0} false
+                ret
+
+# logical-or of r0 and r1
+# overwrites r0 with result
+stl.or:         cmp {r0} true
+                jie stl.or.true
+                cmp {r1} true
+                jie stl.or.true
+                mov {r0} false
+                ret
+stl.or.true:    mov {r0} true
+                ret
+
+
+"""
+
+if __name__ == "__main__":
+    
+    register_names = ["ra", "rb"]
+    print(stl_template.format(
+        **{("r%d"%r): name for r, name in enumerate(register_names)}))
